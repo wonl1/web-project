c57afa54f0d0d42a11f8b14e1b2d30795b5d1809
jwl51@naver.com <jwl51@naver.com>
2024-05-07 21:15:28 +0900

    navbar2

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,23 +1,26 @@
 <script>
-    import "../app.pcss";
-    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
-   </script>
-   <Navbar let:NavContainer color="primary">
-    <NavContainer class="border px-5 py-2 rounded-lg bg-white dark:bg-gray-600">
-      <NavBrand href="/">
-        <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
-        <span class="self-center whitespace-nowrap text-xl font-semibold">Flowbite</span>
-      </NavBrand>
-      <NavHamburger />
-      <NavUl>
-        <NavLi href="/">Home</NavLi>
-        <NavLi href="/about">지역</NavLi>
-        <NavLi href="/docs/components/navbar">계절</NavLi>
-        <NavLi href="/pricing">테마</NavLi>
-        <NavLi href="/contact">이벤트</NavLi>
-      </NavUl>
-    </NavContainer>
-  </Navbar>
-   <div class="px-2 sm:px-4"><div class="container mx-auto">
-   <slot></slot>
+  import "../app.pcss";
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+  import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+  import { faCamera } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<Navbar let:NavContainer color="blue">
+  <NavContainer class="border px-5 py-2 rounded-lg bg-white dark:bg-gray-600">
+    <NavBrand href="/">
+      <FontAwesomeIcon icon={faCamera} size="2x" style="margin-right: 8px;" /> 
+      <span class="self-center whitespace-nowrap text-xl font-semibold">Travel Sharing</span>
+    </NavBrand>
+    <NavHamburger />
+    <NavUl>
+      <NavLi href="/">Home</NavLi>
+      <NavLi href="/지역">지역</NavLi>
+      <NavLi href="/계절">계절</NavLi>
+      <NavLi href="/테마">테마</NavLi>
+      <NavLi href="/이벤트">이벤트</NavLi>
+    </NavUl>
+  </NavContainer>
+</Navbar>
+<div class="px-2 sm:px-4"><div class="container mx-auto">
+<slot></slot>
    </div></div>
