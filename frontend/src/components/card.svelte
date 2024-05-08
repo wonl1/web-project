70dcd91e96dc36c1ddc38866f6250d839e0aa2a3
jwl51@naver.com <jwl51@naver.com>
2024-05-08 10:49:53 +0900

    card

---
+++ frontend/src/components/card.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { Card, Button } from 'flowbite-svelte';
+    
+    let vCard = false;
+</script>
+  
+<div class="flex space-x-4">
+    {#each [1, 2, 3, 4] as _}
+        <Card reverse={vCard} class="flex-1">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          <textarea class="mb-3 p-2 w-full h-20 border rounded-md" placeholder="Enter your text here"></textarea>
+          <Button>
+            Read more
+          </Button>
+        </Card>
+    {/each}
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script>
     import Carousel from '/bce/lectures/2024-web-application-programming/won51/web-project/frontend/src/components/carousel.svelte';
+    import Card from '/bce/lectures/2024-web-application-programming/won51/web-project/frontend/src/components/card.svelte'; 
+    
     let data = "Hello world!";
 </script>
 
 <Carousel />
+
+<div class="mt-4"></div>
+
+<Card />
 
 <b>{data}</b>
