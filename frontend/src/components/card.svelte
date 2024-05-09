f12c3b33d9799e67556a89d8246e684d591bc8d2
jwl51@naver.com <jwl51@naver.com>
2024-05-09 23:52:56 +0900

    card2

--- frontend/src/components/card.svelte
+++ frontend/src/components/card.svelte
@@ -1,18 +1,18 @@
 <script>
-    import { Card, Button } from 'flowbite-svelte';
-    
-    let vCard = false;
-</script>
+  import { Card, Button } from 'flowbite-svelte';
   
-<div class="flex space-x-4">
-    {#each [1, 2, 3, 4] as _}
-        <Card reverse={vCard} class="flex-1">
+  let vCard = false;
+</script>
+
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+  {#each [1, 2, 3, 4] as _}
+      <Card reverse={vCard} class="flex-1">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
           <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
           <textarea class="mb-3 p-2 w-full h-20 border rounded-md" placeholder="Enter your text here"></textarea>
           <Button>
-            Read more
+              Read more
           </Button>
-        </Card>
-    {/each}
+      </Card>
+  {/each}
 </div>
